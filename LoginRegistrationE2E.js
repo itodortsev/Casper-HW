f388ef969921c48f9b9a31722da827812d926045
itodortsev <itodortsev@rocketlawyer.com>
2014-12-04 21:23:34 -0800

    Update LoginRegistrationE2E.js

--- LoginRegistrationE2E.js
+++ LoginRegistrationE2E.js
@@ -4,10 +4,26 @@ casper.test.begin('Login and Registration End to End Test', function suite(test)
     casper.start( urlPrefix + ".rocketlawyer.com", function() {
         this.echo("======= Beginning Test Suite =======");
         test.assertHttpStatus(200, 'Connected to Landing page');
-        test.assertTitle('Rocket Lawyer', 'Title is correct');
+
+    // can't remember where to look for 'Affordable Legal Services...'
+        test.assertTitle("Affordable Legal Services, Free Legal Documents, Advice & Ask a Lawyer | Rocket Lawyer", 'Title is correct');
     });
 
-    casper.run(function() {
+    //Click the Sign Up button in the navigation bar
+    casper.click ("/login-register.rl#/register?hd=navreg" class="rlHeaderTopRightLink" {
         test.done();
     });
-});
+
+    // how do I find an element for Continue button?
+
+    //Register a new user
+    "user is: "+username+"\n"
+
+    //Confirm that the user is at the Dashboard after registration
+    "rlHeaderTopRightAccountInfoNameEtc ng-binding"
+
+    //log the user out
+}); "rlHeaderTopIconProfileImage img" ng-src="/refresh_assets/img/navImages/avatarDefault.png"
+
+    //how do I find an element for Sign Out option?
+
