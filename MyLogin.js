9f42c4be679fade1fec84a999aef3d8f1b385684
itodortsev <itodortsev@rocketlawyer.com>
2014-12-05 14:36:18 -0800

    Update MyLogin.js

--- MyLogin.js
+++ MyLogin.js
@@ -6,7 +6,7 @@ casper.test.begin('Login and Registration End to End Test', function suite(test)
         test.assertHttpStatus(200, 'Connected to Landing page');
         test.assertTitle('Affordable Legal Services, Free Legal Documents, Advice & Ask a Lawyer | Rocket Lawyer', 'Title is correct');
     });
-  casper.thenClick(".rlHeaderTopRightLink[href='/login-register.rl#/register?hd=navreg'", function() {
+  casper.thenClick(".rlHeaderTopRightLink[href='/login-register.rl#/register?hd=navreg']", function() {
   this.echo("=======Registering a user========");
   });
     casper.run(function() {
