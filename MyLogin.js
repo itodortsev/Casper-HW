75f155eb8eaa75dd405c27ce8f539e02e5cf28b4
itodortsev <itodortsev@rocketlawyer.com>
2014-12-09 10:59:57 -0800

    Update MyLogin.js

--- MyLogin.js
+++ MyLogin.js
@@ -1,16 +1,41 @@
 casper.test.begin('Login and Registration End to End Test', function suite(test) {
+var startingUrl = getStartingPoint("/homepage.rl");
+var userName = "itodortest" + Math.random() + "@gmail.com";
+var password = userName;
 
-    // Load the landing page and verify it is correct
-    casper.start("https://www.rocketlawyer.com", function() {
+
+    // =========Load the landing page and verify it is correct=========//
+  casper.start("https://www.rocketlawyer.com", function() {
         this.echo("======= Beginning Test Suite =======");
         test.assertHttpStatus(200, 'Connected to Landing page');
         test.assertTitle('Affordable Legal Services, Free Legal Documents, Advice & Ask a Lawyer | Rocket Lawyer', 'Title is correct');
     });
+
+    // =========Click on Sign Up link in the header============///
   casper.thenClick(".rlHeaderTopRightLink[href='/login-register.rl#/register?hd=navreg']", function() {
-  this.echo("=======Registering a user========");
+  this.echo("going to the registration page");
+
+    // =========Verify that user is redirected to Registration page=============//
+  casper.waitForUrl("/login-register.rl", function () {
+    test.assertTitle("Login and registration", "Title matches the registration page");
+  
+  this.fillSelectors("form#registerForm", {
+    "form#registerForm #email"  : userName,
+    "form#registerForm #pass"   : userName
+  }, true);
+
+
+
+  
+
+
+
+
   });
     casper.run(function() {
         test.done();
     });
 });
+
+
 
